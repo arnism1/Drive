7a99c602b8a64653fd6a56746a6c9488030ba998
arnism <miftari@kufgem.at>
2024-01-22 09:47:27 +0100

    No errors but dont accept user with new fields as registration

--- Drive/Data/ApplicationUser.cs
+++ Drive/Data/ApplicationUser.cs
@@ -5,6 +5,8 @@ namespace Drive.Data
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        public string Vorname { get; set; }
+        public string Nachname { get; set; }
+        public string MitarbeiterID { get; set; }
     }
-
 }
--- Drive/Data/Migrations/20240122084333_ExtendIdentity.Designer.cs
+++ Drive/Data/Migrations/20240122084333_ExtendIdentity.Designer.cs
@@ -4,6 +4,7 @@ using Drive.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Drive.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240122084333_ExtendIdentity")]
+    partial class ExtendIdentity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
